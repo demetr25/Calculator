9f30242521f235ca416ccf4bf8c3cf3eecef2e97
Admin <dmitrijdundakov@gmail.com>
2021-10-13 18:41:07 +0300

    repear

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,18 +7,19 @@ public class Main {
     public static void main(String[] args) {
         char p ='1';
         while(p!='e'){
-            System.out.print("Hey you, piece of shit! Input a number here -> ");
+            System.out.print("Input a number here -> ");
             Scanner scanner = new Scanner(System.in);
             int num1 = scanner.nextInt();
-            System.out.print("Hey you, piece of shit! Input the second number here -> ");
+            System.out.print("Input the second number here -> ");
             int num2 = scanner.nextInt();
-            System.out.print("Choose the motherfucking operation -> ");
+            System.out.print("Choose the operation -> ");
             p = scanner.next().charAt(0);
             switch (p) {
                 case '+' -> System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
                 case '-' -> System.out.printf("%d - %d = %d\n", num1, num2, num1 - num2);
                 case '/' -> System.out.printf("%d / %d = %d\n", num1, num2, num1 / num2);
                 case '*' -> System.out.printf("%d * %d = %d\n", num1, num2, num1 * num2);
+                case '^' -> System.out.printf("%d ^ %d = %f\n", num1, num2, Math.pow(num1, num2));
                 //default -> System.out.print("You did not choose the operation");
         }
         }
